5cf962916bef970bb3cd0c550eceb9364d6b597c
Lora <lorageo33@gmail.com>
2026-03-18 22:19:07 +0200

    initial commit

--- src/practice12/Factorial.java
+++ src/practice12/Factorial.java
@@ -2,14 +2,12 @@ package practice12;
 
 public class Factorial {
 
-    public int factor(int x){
+    public int factor(int x) {
         int factorial = 1;
 
         for (int i = 1; i <= x; i++) {
-
-             factorial = factorial * i;
-
-            }
+            factorial = factorial * i;
+        }
         return factorial;
     }
 }
--- src/practice12/TextUtil.java
+++ src/practice12/TextUtil.java
@@ -2,19 +2,15 @@ package practice12;
 
 public class TextUtil {
 
-    public String textWithoutSpecialSymbols(String text){
-
+    public String textWithoutSpecialSymbols(String text) {
         String resultText = "";
+
         for (int i = 0; i < text.length(); i++) {
-
             char currentChar = text.charAt(i);
-
-            if (currentChar != '(' && currentChar != ')' && currentChar != '@'){
-            resultText = resultText + currentChar;
+            if (currentChar != '(' && currentChar != ')' && currentChar != '@') {
+                resultText = resultText + currentChar;
+            }
         }
-
-    }
         return resultText;
-
-}
+    }
 }
